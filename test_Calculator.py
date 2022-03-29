3d2b97a8de4df73462d1b0cdca747ecfcc701ec4
khushi173 <khushijamwal173@gmail.com>
2022-03-29 17:12:48 +0530

    json report

--- test_Calculator.py
+++ test_Calculator.py
@@ -1,10 +1,10 @@
 import Calculator
 import pytest
 @pytest.mark.xfail
-@pytest.mark.parametrize("a,b,c",[(3,2,5),(10,2,16),(7,8,15),(2,5,8)])
+@pytest.mark.parametrize("a,b,c",[(3,2,5),(10,2,12),(7,8,15),(2,5,8)])
 def test_add(a,b,c):
     result = Calculator.add(a,b)
-    assert c ==result
+    assert not c ==result
 @pytest.mark.parametrize("a,b,c",[(10,2,8),(10,2,18),(8,7,2),(12,5,7)])
 def test_sub(a,b,c):
     result = Calculator.sub(a,b)
